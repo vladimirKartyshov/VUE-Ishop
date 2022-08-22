af98742afb55f7931016bcdd2bedbf2059bba693
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-08-22 04:24:17 +0300

    block3 task2

---
+++ src/data.js
@@ -0,0 +1,6 @@
+const data = {
+  message: 'Hello world',
+  message2: 'Hello world 2',
+}
+
+export default data
--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import data from './data'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app')
+
+alert(data.message)
+alert(data.message2)
