463ad5936994a31cdced6b99847dba01018006aa
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-08-25 01:43:17 +0300

    block 3 task 3

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Sumsung',
+    price: 1000,
+    image: '/img/my-img/product-7.png',
+  },
+  {
+    title: 'Iphone',
+    price: 2000,
+    image: '/img/my-img/product-6.png',
+  },
+  {
+    title: 'HTC',
+    price: 3000,
+    image: '/img/my-img/product-5.png',
+  },
+  {
+    title: 'HTC X',
+    price: 3200,
+    image: '/img/my-img/product-4.png',
+  },
+  {
+    title: 'HTC 2X',
+    price: 3500,
+    image: '/img/my-img/product-3.png',
+  },
+  {
+    title: 'Sumsung X',
+    price: 4000,
+    image: '/img/my-img/product-2.png',
+  },
+  {
+    title: 'HUEAWAY',
+    price: 3000,
+    image: '/img/my-img/product-1.png',
+  },
+  {
+    title: 'Player',
+    price: 1100,
+    image: '/img/my-img/product-8.jpg',
+  },
+  {
+    title: 'Alcatel',
+    price: 1500,
+    image: '/img/my-img/product-9.jpg',
+  },
+  {
+    title: 'Garmin',
+    price: 4500,
+    image: '/img/my-img/product-10.jpg',
+  },
+  {
+    title: 'Xiomy',
+    price: 3000,
+    image: '/img/my-img/product-11.jpg',
+  },
+  {
+    title: 'Kindle',
+    price: 1000,
+    image: '/img/my-img/product-12.jpg',
+  },
+]
