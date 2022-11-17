4c914e92fe94fb3700ab7d21d2b17a47e8b59507
Vladimir Kartyshov <Kratos@beelinerouter.net>
2022-11-17 15:48:42 +0300

    task 7.5 completed

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -14,6 +14,19 @@
       ></app-product-filter>
 
       <section class="catalog">
+        <div v-if="productsLoading">
+          <div class="spinner-container">
+            <div class="spinner"></div>
+          </div>
+          <h3 class="textloadcolor">Загрузка товаров....</h3>
+        </div>
+        <div v-if="productsLoadingFailed">
+          Произошла ошибка при загрузке!!!
+          <button class="trybtn" @click.prevent="loadProducts">
+            Try again
+          </button>
+        </div>
+
         <app-product-list :products="products"></app-product-list>
 
         <app-base-pagination
@@ -52,6 +65,9 @@ export default {
       productsPerPage: 6,
 
       productsData: null,
+
+      productsLoading: false,
+      productsLoadingFailed: false,
     }
   },
 
@@ -106,6 +122,8 @@ export default {
 
   methods: {
     loadProducts() {
+      this.productsLoading = true
+      this.productsLoadingFailed = false
       clearTimeout(this.loadProductsTimer)
       this.loadProductsTimer = setTimeout(() => {
         axios
@@ -120,7 +138,9 @@ export default {
             },
           })
           .then((response) => (this.productsData = response))
-      }, 0)
+          .catch(() => (this.productsLoadingFailed = true))
+          .then(() => (this.productsLoading = false))
+      }, 2000)
     },
   },
 
@@ -148,4 +168,64 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.trybtn {
+  background-color: #9eff00;
+  border: 1px solid #9eff00;
+  font-family: 'PressStart';
+  color: #222;
+  font-size: 20px;
+  margin-left: 70px;
+  height: 45px;
+  cursor: pointer;
+}
+.spinner-container {
+  width: 100%;
+  height: 100%;
+  z-index: 99999;
+  text-align: center;
+  background-color: #ffffff;
+  position: fixed;
+}
+.spinner {
+  margin: auto;
+  border: 6px solid #dbf2ff;
+
+  width: 62px;
+  height: 62px;
+  display: inline-block;
+  position: absolute;
+  top: -5%;
+  left: 20%;
+  border-radius: 50%;
+  border-right: 6px solid #018df7;
+  text-align: center;
+  animation-name: spin;
+  animation-duration: 900ms;
+  animation-iteration-count: infinite;
+  /*animation-timing-function: linear; /*linear spin */
+  animation-timing-function: cubic-bezier(0.53, 0.21, 0.29, 0.67);
+}
+@-webkit-keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  50% {width:24px;height:24px} /*shrinking effect
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+/* @keyframes spin {
+  0% {
+    -webkit-transform: rotate(0deg);
+  }
+  /* 50% {width:24px;height:24px} shrinking effect */
+  100% {
+    -webkit-transform: rotate(360deg);
+  }
+}
+
+.textloadcolor {
+  color: #018df7;
+}
+</style>
